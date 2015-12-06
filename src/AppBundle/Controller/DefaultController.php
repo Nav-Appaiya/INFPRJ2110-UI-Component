e861dc11b18c8ddd7833be80615ca88f0ddf479d
Nav Appaiya <navarajh@gmail.com>
2015-12-06 19:03:47 +0100

    Login screen finished

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,14 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->render('AppBundle:Master:index.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        return $this->render('AppBundle:Master:login.html.twig');
     }
 }
